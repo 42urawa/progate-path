5dea498008703a66ff242c43b8c94e6dcab91853
42urawa <yuichiro.makita24@gmail.com>
2022-05-28 13:29:09 +0900

    test

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -1,12 +1,12 @@
 // `n` must be integer (not floating point)
-export let fibonacci = (n: number) => {
-  if(n <= 0) return 0;
+export const fibonacci = (n: number): number => {
+  if (n <= 0) return 0;
   let prev = 0;
   let now = 1;
   for (let i = 2; i <= n; ++i) {
-    const newValue = prev + now
-    prev = now
-    now = newValue
+    const newValue = prev + now;
+    prev = now;
+    now = newValue;
   }
-  return now
+  return now;
 };
